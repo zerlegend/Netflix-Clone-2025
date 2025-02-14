2c2f2d47d5c4d6e07be5d4a40f1b72e6a4e78823
zerlegend <zerlegend@gmail.com>
2025-02-14 10:29:54 +0200

    header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-
-import './App.css'
+import "./App.css";
+import Home from "./Pages/Home/Home";
 
 function App() {
-
   return (
     <>
-      <h1>Hello</h1>
+      <div className="App">
+        <Home />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Footer from "../../Components/Footer/Footer"
+import Header from "../../Components/Header/Header"
+
+function Home() {
+  return (
+    <>
+      <Header/>
+      <Footer/>
+    </>
+  )
+}
+
+export default Home
