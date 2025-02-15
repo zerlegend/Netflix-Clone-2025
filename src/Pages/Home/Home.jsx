7bc5f6c53e733f21b16fc52eac0950411061b879
zerlegend <zerlegend@gmail.com>
2025-02-15 06:36:48 +0200

    banner

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import Footer from "../../Components/Footer/Footer"
 import Header from "../../Components/Header/Header"
+import Banner from "../../Components/Banner/Banner"
 
 function Home() {
   return (
     <>
       <Header/>
+      <Banner/>
       <Footer/>
     </>
   )
